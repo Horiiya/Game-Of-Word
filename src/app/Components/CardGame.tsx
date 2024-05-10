a22e5f63df16ca806a85128ccd165eb5f09d3d07
NichaToon <nichapat.uah@gmail.com>
2024-05-10 22:20:01 +0700

    fix bug of choose incorrect it can choose correct and up score

--- src/app/Components/CardGame.tsx
+++ src/app/Components/CardGame.tsx
@@ -239,6 +239,234 @@
 
 
 
+
+// import React, { useState, useEffect } from 'react';
+// import Picture from './Picture';
+// import Header from './Header';
+// import Button from './Button';
+// import ModalComponents from './Modal';
+// import Data from '../Data.json';
+// import { useRouter } from 'next/navigation';
+
+// interface CardGameProps {
+//   questionProps: number;
+//   srcProps: string;
+//   altProps: string;
+//   nameOfButton1Props: string;
+//   nameOfButton2Props: string;
+//   nameOfButton3Props: string;
+//   nameOfButton4Props: string;
+//   descriptionProps: string;
+// }
+
+// export function CardGame({
+//   questionProps,
+//   srcProps,
+//   altProps,
+//   nameOfButton1Props,
+//   nameOfButton2Props,
+//   nameOfButton3Props,
+//   nameOfButton4Props,
+//   descriptionProps
+// }: CardGameProps) {
+//   const localStorageKey = 'answeredQuestions';
+//   const initialAnsweredQuestions = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
+//   const [answeredQuestions, setAnsweredQuestions] = useState<number[]>(initialAnsweredQuestions);
+
+//   const [score, setScore] = useState<number>(() => {
+//     const storedScore = localStorage.getItem('score');
+//     return storedScore ? parseInt(storedScore, 10) : 0;
+//   });
+
+//   const [modalTrueIsOpen, setModalTrueIsOpen] = useState<boolean>(false);
+//   const [modalFalseIsOpen, setModalFalseIsOpen] = useState<boolean>(false);
+
+//   useEffect(() => {
+//     localStorage.setItem('score', score.toString());
+//   }, [score]);
+
+//   const handleButtonClick = (isCorrect: boolean) => {
+//     if (isCorrect) {
+//       if (!answeredQuestions.includes(questionProps)) {
+//         const newScore: number = score + 1;
+//         setScore(newScore);
+//         const updatedQuestions = [...answeredQuestions, questionProps];
+//         setAnsweredQuestions(updatedQuestions);
+//         localStorage.setItem(localStorageKey, JSON.stringify(updatedQuestions));
+//       }
+//       setModalTrueIsOpen(true);
+//     } else {
+//       setModalFalseIsOpen(true);
+      
+//     }
+//   };
+
+//   const filteredData = Data.filter((post) => post.id === questionProps);
+//   const post = filteredData[0];
+
+//   const checkChoice = (choiceTest: string) => {
+//     return choiceTest === post.answer;
+//   };
+
+//   return (
+//     <main>
+//       <Header />
+
+//       <div className='flex justify-end mr-[1.5rem]'>
+//         <div className='bg-resultDiv w-[10rem] rounded-[2rem] h-[3rem] flex justify-center'>
+//           <p className="text-3xl p-[0.4rem] items-end">Score: {score}</p>
+//         </div>
+//       </div>
+//       <p className="text-3xl text-center">Question {questionProps}:</p>
+//       <Picture src={srcProps} alt={altProps} description={descriptionProps} />
+//       <div className="flex bg-white-500 flex justify-center gap-6">
+//         <Button name={nameOfButton1Props} fnOnClick={() => handleButtonClick(checkChoice(nameOfButton1Props))} />
+//         <Button name={nameOfButton2Props} fnOnClick={() => handleButtonClick(checkChoice(nameOfButton2Props))} />
+//         <Button name={nameOfButton3Props} fnOnClick={() => handleButtonClick(checkChoice(nameOfButton3Props))} />
+//         <Button name={nameOfButton4Props} fnOnClick={() => handleButtonClick(checkChoice(nameOfButton4Props))} />
+
+//         <ModalComponents
+//           nameOfModal="It's True"
+//           state={modalTrueIsOpen}
+//           setModal={setModalTrueIsOpen}
+//           score={score}
+//           question={questionProps}
+//         />
+//         <ModalComponents
+//           nameOfModal="It's False"
+//           state={modalFalseIsOpen}
+//           setModal={setModalFalseIsOpen}
+//           score={score}
+//           question={questionProps}
+//         />
+//       </div>
+//       <br className="word-break: break-all" />
+//       <div className="flex bg-white-500 h-[5.8rem]"></div>
+//     </main>
+//   );
+// }
+
+// export default CardGame;
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import Picture from './Picture';
+// import Header from './Header';
+// import Button from './Button';
+// import ModalComponents from './Modal';
+// import Data from '../Data.json';
+// import { useRouter } from 'next/navigation';
+
+// interface CardGameProps {
+//   questionProps: number;
+//   srcProps: string;
+//   altProps: string;
+//   nameOfButton1Props: string;
+//   nameOfButton2Props: string;
+//   nameOfButton3Props: string;
+//   nameOfButton4Props: string;
+//   descriptionProps: string;
+// }
+
+// export function CardGame({
+//   questionProps,
+//   srcProps,
+//   altProps,
+//   nameOfButton1Props,
+//   nameOfButton2Props,
+//   nameOfButton3Props,
+//   nameOfButton4Props,
+//   descriptionProps
+// }: CardGameProps) {
+//   const localStorageKey = 'answeredQuestions';
+//   const initialAnsweredQuestions = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
+//   const [answeredQuestions, setAnsweredQuestions] = useState<number[]>(initialAnsweredQuestions);
+
+//   const [score, setScore] = useState<number>(() => {
+//     const storedScore = localStorage.getItem('score');
+//     return storedScore ? parseInt(storedScore, 10) : 0;
+//   });
+
+//   const [modalTrueIsOpen, setModalTrueIsOpen] = useState<boolean>(false);
+//   const [modalFalseIsOpen, setModalFalseIsOpen] = useState<boolean>(false);
+
+//   useEffect(() => {
+//     localStorage.setItem('score', score.toString());
+//   }, [score]);
+
+//   const handleButtonClick = (isCorrect: boolean) => {
+//     if (isCorrect) {
+//       if (!answeredQuestions.includes(questionProps)) {
+//         const newScore: number = score + 1;
+//         setScore(newScore);
+//         const updatedQuestions = [...answeredQuestions, questionProps];
+//         setAnsweredQuestions(updatedQuestions);
+//         localStorage.setItem(localStorageKey, JSON.stringify(updatedQuestions));
+//       }
+//       setModalTrueIsOpen(true);
+//     } else {
+//       setModalFalseIsOpen(true);
+      
+//     }
+//   };
+
+//   const filteredData = Data.filter((post) => post.id === questionProps);
+//   const post = filteredData[0];
+
+//   const checkChoice = (choiceTest: string) => {
+//     return choiceTest === post.answer;
+//   };
+
+//   return (
+//     <main>
+//       <Header />
+
+//       <div className='flex justify-end mr-[1.5rem]'>
+//         <div className='bg-resultDiv w-[10rem] rounded-[2rem] h-[3rem] flex justify-center'>
+//           <p className="text-3xl p-[0.4rem] items-end">Score: {score}</p>
+//         </div>
+//       </div>
+//       <p className="text-3xl text-center">Question {questionProps}:</p>
+//       <Picture src={srcProps} alt={altProps} description={descriptionProps} />
+//       <div className="flex bg-white-500 flex justify-center gap-6">
+//         <Button name={nameOfButton1Props} fnOnClick={() => handleButtonClick(checkChoice(nameOfButton1Props))} />
+//         <Button name={nameOfButton2Props} fnOnClick={() => handleButtonClick(checkChoice(nameOfButton2Props))} />
+//         <Button name={nameOfButton3Props} fnOnClick={() => handleButtonClick(checkChoice(nameOfButton3Props))} />
+//         <Button name={nameOfButton4Props} fnOnClick={() => handleButtonClick(checkChoice(nameOfButton4Props))} />
+
+//         <ModalComponents
+//           nameOfModal="It's True"
+//           state={modalTrueIsOpen}
+//           setModal={setModalTrueIsOpen}
+//           score={score}
+//           question={questionProps}
+//         />
+//         <ModalComponents
+//           nameOfModal="It's False"
+//           state={modalFalseIsOpen}
+//           setModal={setModalFalseIsOpen}
+//           score={score}
+//           question={questionProps}
+//         />
+//       </div>
+//       <br className="word-break: break-all" />
+//       <div className="flex bg-white-500 h-[5.8rem]"></div>
+//     </main>
+//   );
+// }
+
+// export default CardGame;
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import Picture from './Picture';
 import Header from './Header';
@@ -258,6 +486,7 @@ interface CardGameProps {
   descriptionProps: string;
 }
 
+
 export function CardGame({
   questionProps,
   srcProps,
@@ -269,8 +498,12 @@ export function CardGame({
   descriptionProps
 }: CardGameProps) {
   const localStorageKey = 'answeredQuestions';
-  const initialAnsweredQuestions = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
-  const [answeredQuestions, setAnsweredQuestions] = useState<number[]>(initialAnsweredQuestions);
+  const initialAnsweredQuestions = JSON.parse(localStorage.getItem(localStorageKey) || '{}');
+  const [answeredQuestions, setAnsweredQuestions] = useState<{ [key: number]: boolean }>(initialAnsweredQuestions);
+
+  const initialAnswerOnce = JSON.parse(localStorage.getItem('answeredOnce') || '{}');
+  const [answeredOnce, setAnsweredOnce] = useState<{ [key: number]: boolean }>(initialAnswerOnce);
+
   const [score, setScore] = useState<number>(() => {
     const storedScore = localStorage.getItem('score');
     return storedScore ? parseInt(storedScore, 10) : 0;
@@ -284,16 +517,65 @@ export function CardGame({
   }, [score]);
 
   const handleButtonClick = (isCorrect: boolean) => {
+   
+    // if (isCorrect && !answeredOnce[questionProps] && !answeredQuestions[questionProps]) {
+
+      
+    //     const newScore: number = score + 1;
+    //     setScore(newScore);
+    //     const updatedQuestions = { ...answeredQuestions, [questionProps]: true };
+    //     setAnsweredQuestions(updatedQuestions);
+    //     localStorage.setItem(localStorageKey, JSON.stringify(updatedQuestions));
+
+    //     const updatedAnswerOnce = { ...answeredOnce, [questionProps]: true };
+    //     setAnsweredOnce(updatedAnswerOnce);
+    //     localStorage.setItem('answeredOnce', JSON.stringify(updatedAnswerOnce));
+
+    //     setModalTrueIsOpen(true);
+    // } 
+    // else if (isCorrect && !answeredQuestions[questionProps]) {
+    //   const updatedQuestions = { ...answeredQuestions, [questionProps]: true };
+    //   setAnsweredQuestions(updatedQuestions);
+    //   localStorage.setItem(localStorageKey, JSON.stringify(updatedQuestions));
+    //   setModalTrueIsOpen(true);
+    // }
+    // else if (isCorrect && !answeredOnce[questionProps]) {
+    //   const updatedAnswerOnce = { ...answeredOnce, [questionProps]: true };
+    //     setAnsweredOnce(updatedAnswerOnce);
+    //     localStorage.setItem('answeredOnce', JSON.stringify(updatedAnswerOnce));
+    // }
+
     if (isCorrect) {
-      if (!answeredQuestions.includes(questionProps)) {
+      if(!answeredOnce[questionProps] && !answeredQuestions[questionProps]) {
         const newScore: number = score + 1;
         setScore(newScore);
-        const updatedQuestions = [...answeredQuestions, questionProps];
+        const updatedQuestions = { ...answeredQuestions, [questionProps]: true };
         setAnsweredQuestions(updatedQuestions);
         localStorage.setItem(localStorageKey, JSON.stringify(updatedQuestions));
+
+        const updatedAnswerOnce = { ...answeredOnce, [questionProps]: true };
+        setAnsweredOnce(updatedAnswerOnce);
+        localStorage.setItem('answeredOnce', JSON.stringify(updatedAnswerOnce));
+
+        setModalTrueIsOpen(true);
       }
-      setModalTrueIsOpen(true);
-    } else {
+      else {
+        setModalTrueIsOpen(true);
+      }
+      
+      
+  } 
+
+
+    // ตอบผิด ปัดไปนี่
+    else {
+        const updatedQuestions = { ...answeredQuestions, [questionProps]: true };
+        setAnsweredQuestions(updatedQuestions);
+        localStorage.setItem(localStorageKey, JSON.stringify(updatedQuestions));
+
+        const updatedAnswerOnce = { ...answeredOnce, [questionProps]: true };
+        setAnsweredOnce(updatedAnswerOnce);
+        localStorage.setItem('answeredOnce', JSON.stringify(updatedAnswerOnce));
       setModalFalseIsOpen(true);
     }
   };
@@ -339,11 +621,9 @@ export function CardGame({
       </div>
       <br className="word-break: break-all" />
       <div className="flex bg-white-500 h-[5.8rem]"></div>
+      
     </main>
   );
 }
 
 export default CardGame;
-
-
-
--- src/app/question4/page.tsx
+++ src/app/question4/page.tsx
@@ -1,119 +1,119 @@
-// 'use client'
-// import Image from "next/image";
-// import ListeningImg from "../app/ImageOfWord/listening.jpg";
-// import React from 'react';
-// import { useState } from 'react'
-// import ReactModal from 'react-modal';
-// // import { useRouter } from 'next/router';
-// import Link from 'next/link';
-// import { useRouter } from 'next/navigation';
-// import Picture from "../Components/Picture";
-// import Header from "../Components/Header";
-// import Button from "../Components/Button";
-// import ModalComponents from "../Components/Modal"; 
+// // 'use client'
+// // import Image from "next/image";
+// // import ListeningImg from "../app/ImageOfWord/listening.jpg";
+// // import React from 'react';
+// // import { useState } from 'react'
+// // import ReactModal from 'react-modal';
+// // // import { useRouter } from 'next/router';
+// // import Link from 'next/link';
+// // import { useRouter } from 'next/navigation';
+// // import Picture from "../Components/Picture";
+// // import Header from "../Components/Header";
+// // import Button from "../Components/Button";
+// // import ModalComponents from "../Components/Modal"; 
 
-// export default function Page({ searchParams } : {
-//   searchParams: {
-//     score : number;
-//   };
+// // export default function Page({ searchParams } : {
+// //   searchParams: {
+// //     score : number;
+// //   };
 
   
-// }) {
-//   console.log(searchParams.score)
-//   const score1 = searchParams.score
+// // }) {
+// //   console.log(searchParams.score)
+// //   const score1 = searchParams.score
  
-//   return (
-//     <div>
-//       <Link href="/page2">This is page 2</Link>
-//       <p>this is my score{score1}</p>
-//       <p className="text-blue-500 text-3xl text-center">Game Of Word</p>
-//       <p className="text-blue-500 text-3xl text-center">what is this word?</p>
-//       <p className="text-blue-500 text-3xl text-right">Score : {score1}</p>
-//     </div>
-//   )
-// }
+// //   return (
+// //     <div>
+// //       <Link href="/page2">This is page 2</Link>
+// //       <p>this is my score{score1}</p>
+// //       <p className="text-blue-500 text-3xl text-center">Game Of Word</p>
+// //       <p className="text-blue-500 text-3xl text-center">what is this word?</p>
+// //       <p className="text-blue-500 text-3xl text-right">Score : {score1}</p>
+// //     </div>
+// //   )
+// // }
 
 
 
-// 'use client'
-// import React from 'react';
-// import { useState } from 'react'
-// import CardGame from "../Components/CardGame"; 
-// import Data from '../Data.json';
+// // 'use client'
+// // import React from 'react';
+// // import { useState } from 'react'
+// // import CardGame from "../Components/CardGame"; 
+// // import Data from '../Data.json';
 
-// export default function Home() {
-//   const [score, setScore] = useState<number>(0);
-//   const filteredData = Data.filter((post) => post.id === 2);
-//   const post = filteredData[0]; // Assuming you only want to display the first post
+// // export default function Home() {
+// //   const [score, setScore] = useState<number>(0);
+// //   const filteredData = Data.filter((post) => post.id === 2);
+// //   const post = filteredData[0]; // Assuming you only want to display the first post
 
-//   return(
-//     <main>
-//       <div key={post.id}>
-//       <CardGame 
-//         scoreProps={score} 
-//         questionProps={post.id}
-//         srcProps={post.src} 
-//         altProps={post.alt}
-//         descriptionProps={post.question}
-//         nameOfButton1Props={post.choice1} 
-//         nameOfButton2Props={post.choice2} 
-//         nameOfButton3Props={post.choice3}
-//         nameOfButton4Props={post.choice4}
-//       />
-//       </div>
-//     </main>  
-//   );
-// }
+// //   return(
+// //     <main>
+// //       <div key={post.id}>
+// //       <CardGame 
+// //         scoreProps={score} 
+// //         questionProps={post.id}
+// //         srcProps={post.src} 
+// //         altProps={post.alt}
+// //         descriptionProps={post.question}
+// //         nameOfButton1Props={post.choice1} 
+// //         nameOfButton2Props={post.choice2} 
+// //         nameOfButton3Props={post.choice3}
+// //         nameOfButton4Props={post.choice4}
+// //       />
+// //       </div>
+// //     </main>  
+// //   );
+// // }
 
 
 
-// 'use client'
-// import Image from "next/image";
-// import ListeningImg from "../app/ImageOfWord/listening.jpg";
-// import React from 'react';
-// import { useState } from 'react'
-// import ReactModal from 'react-modal';
-// // import { useRouter } from 'next/router';
-// import Link from 'next/link';
-// import { useRouter } from 'next/navigation';
-// import Picture from "../Components/Picture";
-// import Header from "../Components/Header";
-// import Button from "../Components/Button";
-// import ModalComponents from "../Components/Modal";
-// import CardGame from "../Components/CardGame"; 
-// import Data from '../Data.json'; 
+// // 'use client'
+// // import Image from "next/image";
+// // import ListeningImg from "../app/ImageOfWord/listening.jpg";
+// // import React from 'react';
+// // import { useState } from 'react'
+// // import ReactModal from 'react-modal';
+// // // import { useRouter } from 'next/router';
+// // import Link from 'next/link';
+// // import { useRouter } from 'next/navigation';
+// // import Picture from "../Components/Picture";
+// // import Header from "../Components/Header";
+// // import Button from "../Components/Button";
+// // import ModalComponents from "../Components/Modal";
+// // import CardGame from "../Components/CardGame"; 
+// // import Data from '../Data.json'; 
 
-// export default function Page({ searchParams } : {
-//   searchParams: {
-//     score: string
-//   };
+// // export default function Page({ searchParams } : {
+// //   searchParams: {
+// //     score: string
+// //   };
 
-// }) {
+// // }) {
 
-//   // console.log(searchParams.score)
-//   const scoreTest = Number(searchParams.score)
-//   const [score, setScore] = useState<number>(scoreTest);
-//   const filteredData = Data.filter((post) => post.id === 4);
-//   const post = filteredData[0]; // Assuming you only want to display the first post
+// //   // console.log(searchParams.score)
+// //   const scoreTest = Number(searchParams.score)
+// //   const [score, setScore] = useState<number>(scoreTest);
+// //   const filteredData = Data.filter((post) => post.id === 4);
+// //   const post = filteredData[0]; // Assuming you only want to display the first post
  
-//   return (
-//     <main>
-//       <div key={post.id}>
-//       <p>score test : {score}</p>
-//       <CardGame 
-//         questionProps={post.id}
-//         srcProps={post.src} 
-//         altProps={post.alt}
-//         descriptionProps={post.question}
-//         nameOfButton1Props={post.choice1} 
-//         nameOfButton2Props={post.choice2} 
-//         nameOfButton3Props={post.choice3}
-//         nameOfButton4Props={post.choice4}
-//       />
-//       </div>
-//     </main>  
-//   )
-// }
+// //   return (
+// //     <main>
+// //       <div key={post.id}>
+// //       <p>score test : {score}</p>
+// //       <CardGame 
+// //         questionProps={post.id}
+// //         srcProps={post.src} 
+// //         altProps={post.alt}
+// //         descriptionProps={post.question}
+// //         nameOfButton1Props={post.choice1} 
+// //         nameOfButton2Props={post.choice2} 
+// //         nameOfButton3Props={post.choice3}
+// //         nameOfButton4Props={post.choice4}
+// //       />
+// //       </div>
+// //     </main>  
+// //   )
+// // }
 
 
 
